007b11f284feba67a8bc389cef6171478b048d99
NBerlin <nickiberlinpolhem@gmail.com>
2019-02-19 12:34:57 +0100

    Updates

---
+++ precache-manifest.6c3126bfd6916c70326c022d22682343.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/LearningReact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1777d9bb0f478ef608b4",
+    "url": "/LearningReact/static/js/runtime~main.1777d9bb.js"
+  },
+  {
+    "revision": "9f63ee2b6b8a7a3d851a",
+    "url": "/LearningReact/static/js/main.9f63ee2b.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/LearningReact/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "9f63ee2b6b8a7a3d851a",
+    "url": "/LearningReact/static/css/main.010a029a.chunk.css"
+  },
+  {
+    "revision": "64c87caaeb90d030a6eb1db480e36d84",
+    "url": "/LearningReact/index.html"
+  }
+];
