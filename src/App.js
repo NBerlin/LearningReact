29e210fa0b6d26e2e52c8662d558b7bc63ef162a
NBerlin <nickiberlinpolhem@gmail.com>
2019-03-21 12:17:32 +0100

    We can now see what the time is

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './shine.png';
 import './App.css';
-
 class App extends Component {
   constructor(props){
     super(props);
     this.state={};
   }
+  on
   render() {
     return (
       <div className="App">
@@ -15,10 +16,18 @@ class App extends Component {
           <p>
            <h1>Blip Blip</h1>
           </p>
+          
         </header>
       </div>
     );
   }
 }
+function clock(){
+  const element=(<h1>
+    Klockan är {new Date().toLocaleTimeString()}.
+  </h1>);
+  ReactDOM.render(element,document.getElementById('root'));
+}
+setInterval(clock,1000);
 
 export default App;
