e827e2a2c51141ff4a1f3c67b36335e29ba671d0
NBerlin <nickiberlinpolhem@gmail.com>
2019-03-23 16:45:38 +0100

    added a chessboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import logo from './shine.png';
+import Chessboard from 'chessboardjsx';
 import './App.css';
 class App extends Component {
   constructor(props) {
@@ -28,14 +29,14 @@ class App extends Component {
     this.setState(state=>({
       isToggleOn: !state.isToggleOn
     }));
-  }
+  } 
  
   render() { 
     return (
       <div >
         <h1>Nicki Berlin</h1>
         <h2> It is {this.state.date.toLocaleTimeString()}</h2>
-      
+        <Chessboard position="start" />
       
       <button onClick={this.handleClick}>
         {this.state.isToggleOn ? 'On' : 'Off'}
